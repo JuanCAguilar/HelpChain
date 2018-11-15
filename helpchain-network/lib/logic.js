59c51ad7b37ed564fc560e0dd47815189ecf2d11
JuanCAguilar <juanc.aguilar94@gmail.com>
2018-11-15 02:08:18 -0600

    cosasLogic

--- helpchain-network/lib/logic.js
+++ helpchain-network/lib/logic.js
@@ -10,6 +10,7 @@
  * @param {org.helpchain.SampleTransaction} sampleTransaction
  * @transaction
  */
+
 async function sampleTransaction(tx) {
     // Save the old value of the asset.
     const oldValue = tx.asset.value;
@@ -30,45 +31,95 @@ async function sampleTransaction(tx) {
     emit(event);
 }
 
-
+/////////////////////////////////////////////////////////////////
 
 
 /**
- * Crear Solicitud
- * @param {org.helpchain.CrearSolicitud} crearSolicitud
+ * Funcion que se utiliza para crear una solicitud de un proyecto
+ * @param {org.helpchain.CrearSolicitud} infoSolicitud
  * @transaction
  */
 
- async function crearSolicitud(info){
+ async function CrearSolicitud(infoSolictitud){
+    var NS = 'org.helpchain'
+    var factory = getFactory();
+    //Obtenemos el registro de Usuarios
+    const registroUsuarios = await getParticipantRegistry(NS + '.Usuario');
+    
+    var iD = Math.random().toString(36).substring(3);
+    //Creamos el proyecto
+    var proyecto = factory.newResource(NS, 'Proyecto', iD);
+    proyecto.nombre = infoSolictitud.nombre;
+    proyecto.causa = infoSolictitud.causa;
+    proyecto.meta = infoSolictitud.meta;
+    proyecto.montoMetaTotal = infoSolictitud.montoMetaTotal;
+    proyecto.solicitante = infoSolictitud.solicitante;
+    
+    //Fecha
+    var dateStr = new Date();
+    dateStr = dateStr.toString();
+    proyecto.fechaInicio = dateStr;
+    
+
+
+    //Se crea y emite un evento de regulacion
+    var eventoSolicitud = factory.newEvent(NS, 'getSolicitud');
+    eventoSolicitud.pID = iD;
+    eventoSolicitud.nombre = infoSolictitud.nombre;
+    eventoSolicitud.fechaInicio = dateStr;
+    eventoSolicitud.causa = infoSolictitud.causa;
+    eventoSolicitud.montoMetaTotal = infoSolictitud.montoMetaTotal;
+    eventoSolicitud.solicitante = infoSolictitud.solicitante;
+    emit(eventoSolicitud);
+
+
+    //Agregamos el proyecto al registro de activos
+    const registroProyectos = await getAssetRegistry(NS + '.Proyecto');
+    await registroProyectos.add(proyecto);
+    await registroUsuarios.update(infoSolictitud.solicitante);
 
  }
 
  /**
- * Donar
- * @param {org.helpchain.Donar} donar
+ * Funcion que se utiliza para apoyar un proyecto
+ * @param {org.helpchain.CrearDonativo} infoDonativo
  * @transaction
  */
 
-async function Donar(info){
+async function CrearDonativo(infoDonativo){
+
      
 }
 
 /**
- * Agregar Saldo
- * @param {org.helpchain.AgregarSaldo} agregarSaldo
+ * Agregar una cantidad de dinero a la cuenta de un usuario
+ * @param {org.helpchain.AgregarSaldo} infoUsuario
  * @transaction
  */
 
-async function agregarSaldo(info){
-     
+async function AgregarSaldo(infoUsuario){
+    var NS = 'org.helpchain'
+    var factory = getFactory();
+    //Obtenemos el registro de Usuarios
+    const registroUsuarios = await getParticipantRegistry(NS + '.Usuario');
+    
+    var usuario = infoUsuario.aportante;
+    usuario.saldo += infoUsuario.cantidad;
+
+    var eventoSaldo = factory.newEvent(NS, 'getSaldo');
+    eventoSaldo.cantidad = infoUsuario.cantidad;
+    eventoSaldo.usuario = infoUsuario.aportante;
+    emit(eventoSaldo);
+
+    await registroUsuarios.update(infoUsuario.aportante);
 }
 
 /**
- * Validar Causa
- * @param {org.helpchain.ValidarCausa} validarCausa
+ * Funcion con la que el operador valida y verifica los proyectos
+ * @param {org.helpchain.ValidarProyecto} infoProyecto
  * @transaction
  */
 
-async function validarCausa(info){
+async function ValidarProyecto(infoProyecto){
      
 }
